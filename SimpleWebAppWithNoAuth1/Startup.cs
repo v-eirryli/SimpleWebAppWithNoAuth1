8a2a67cc5b1077cdcea5b0ecc4ed99b47d6fda87
Eirry Li <v-eirryli@microsoft.com>
2021-07-01 02:52:48 -0700

    AGQ

--- SimpleWebAppWithNoAuth1/Startup.cs
+++ SimpleWebAppWithNoAuth1/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SimpleWebAppWithNoAuth1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,7 @@ namespace SimpleWebAppWithNoAuth1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(Configuration.GetConnectionString("BlogConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
